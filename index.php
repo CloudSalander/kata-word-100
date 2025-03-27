c7be7d8d465db0eda45d1bfad9586f126ed84936
CloudSalander <ruben.as532@gmail.com>
2025-03-27 10:31:28 +0100

    Added Finishing Program Condition

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Assuming english alphabet
+*/
+define('POINTS_TO_REACH',100);
+
+$word = readline();
+$wordPoints = 50;
+
+while($wordPoints != POINTS_TO_REACH) {
+    echo "Tip other word";
+    $wordPoints = 100;
+}
+
+
+?>
