967a9bd0fa446f184ccbfebe59a5819511478ceb
CloudSalander <ruben.as532@gmail.com>
2025-03-27 11:02:07 +0100

    What I did in 30 minutes

--- index.php
+++ index.php
@@ -3,13 +3,25 @@
 Assuming english alphabet
 */
 define('POINTS_TO_REACH',100);
+define('VALUE_REFERENCE',ord("z"));
+define('ENGLISH_ALPHABET_LENGTH',25);
 
-$word = readline();
+#$word = readline();
 $wordPoints = 50;
+echo calculateCharPoints(ord("m"));
 
-while($wordPoints != POINTS_TO_REACH) {
-    echo "Tip other word";
-    $wordPoints = 100;
+function calculateWordPoints(string $word): int {
+    $points = 0;
+    $word = strtolower($word);
+    for($i = 0; $i < count($word); ++$i) {
+        $points += calculateCharPoints($word[$i]);
+    }
+    return $points;
+}
+
+
+function calculateCharPoints(string $char): int {
+    return ($char - ord('a'));
 }
 
 
