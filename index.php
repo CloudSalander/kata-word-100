fa7efc19a1e47e22774adf1c37fb6717a3eb1eab
CloudSalander <ruben.as532@gmail.com>
2025-03-31 19:31:24 +0200

    Now calculate word points

--- index.php
+++ index.php
@@ -6,22 +6,21 @@ define('POINTS_TO_REACH',100);
 define('VALUE_REFERENCE',ord("Z"));
 define('ENGLISH_ALPHABET_LENGTH',26);
 
-#$word = readline();
-$wordPoints = 50;
-$test_chars = ["A","B","Z","H"];
-foreach($test_chars as $char) {
-    echo calculateCharPoints($char).PHP_EOL;;
+//$word = readline();
+$words = ["ketchup","Azofaifa","lastre","pericardio","pericardiob"];
+foreach($words as $word) {
+    echo calculateWordPoints($word).PHP_EOL;
 }
-/*
+
 function calculateWordPoints(string $word): int {
     $points = 0;
-    $word = strtolower($word);
-    for($i = 0; $i < count($word); ++$i) {
+    $word = strtoupper($word);
+    for($i = 0; $i < strlen($word); ++$i) {
         $points += calculateCharPoints($word[$i]);
     }
     return $points;
 }
-*/
+
 
 function calculateCharPoints(string $char): int {
     return ENGLISH_ALPHABET_LENGTH - (VALUE_REFERENCE - ord($char));
